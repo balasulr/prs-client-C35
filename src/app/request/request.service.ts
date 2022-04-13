2c0db141d3cd0e16c784854e8399342fd1d781ae
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-04-13 11:01:33 -0400

    Created functions in request.service.ts for requests(int id), review, approve & reject

--- src/app/request/request.service.ts
+++ src/app/request/request.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { request } from 'http';
 import { Observable } from 'rxjs';
 import { Request } from './request.class';
 
@@ -17,6 +18,12 @@ export class RequestService {
   ) { }
 
   // Adding functions:
+
+  // requests(int id) => Retrieves all of the requests in review status but not owned by the user
+  // whose primary key is id. The URL is api/request/reviews/5
+  requests(id: number):Observable<Request[]> {
+    return this.http.get(`${this.baseUrl}/reviews/${id}`) as Observable<Request[]>;
+  }
 
   // list(): get all rows
   list(): Observable<Request[]> {
@@ -36,6 +43,24 @@ export class RequestService {
   // change(request): update
   change(request: Request): Observable<any> {
     return this.http.put(`${this.baseUrl}/${request.id}`, request) as Observable<any>;
+  }
+  
+  // review(int id, Request req) => Sets the request req to review or approved based on
+  // the request total. The URL is api/request/review/5
+  review(request: Request): Observable<any> {
+    return this.http.put(`${this.baseUrl}/review/${request.id}`, request) as Observable<any>;
+  }
+
+  // approve(int id, Request req) => Sets the request req to approved unconditionally.
+  // The URL is api/requests/approve/5.
+  approve(request: Request): Observable<any> {
+    return this.http.put(`${this.baseUrl}/approve/${request.id}`, request) as Observable<any>;
+  }
+  
+  // reject(int id, Request req) => Sets the request req to rejected unconditionally. When rejected, a
+  // request must include text in the rejectionReason property. The URL is api/request/reject/5.
+  reject(request: Request): Observable<any> {
+    return this.http.put(`${this.baseUrl}/reject/${request.id}`, request) as Observable<any>;
   }
 
   // remove(request): delete
